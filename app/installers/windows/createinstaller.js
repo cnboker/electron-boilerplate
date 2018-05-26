2a6e66c4b75a4afd97b58ba35edee09df215b695
cnboker <coobots@gmail.com>
2018-05-26 21:20:53 +0800

    update

--- app/installers/windows/createinstaller.js
+++ app/installers/windows/createinstaller.js
@@ -4,24 +4,24 @@ const path = require('path')
 getInstallerConfig()
   .then(createWindowsInstaller)
   .catch((e) => {
-    console.log(`No dice: ${e.message}`)
+    console.log(e)
     process.exit(1)
   })
 
 function getInstallerConfig () {
   console.log('creating windows installer')
   const rootPath = path.join('./')
-  const outPath = path.join(rootPath, 'release-builds')
+  const outPath = path.join(rootPath, 'release')
   return Promise.resolve({
       //appDirectory: Tells the installer where to look for the packaged app.
-    appDirectory: path.join(outPath, 'kwpolish-app-win32-ia32/'),
+    appDirectory: path.join(outPath, 'kwpolish-win32-ia32/'),
     authors: 'ioliz.com',
     //noMsi: should we use an msi installer?
     noMsi: true,
     //outputDirectory: where should the installer be saved?
     outputDirectory: path.join(outPath, 'windows-installer'),
     //exe: the name of the packaged exe file in the app directory
-    exe: 'kwpolish-app.exe',
+    exe: 'kwpolish.exe',
     //setupExe: the name of the installer exe
     setupExe: 'kwpolishInstaller.exe',
     description:'keyword polish',
