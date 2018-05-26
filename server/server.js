cdc9b3245fa5ce25c0286fcecd9cdcb09e3d9faa
cnboker <coobots@gmail.com>
2018-05-26 21:21:59 +0800

    update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
-app = express(),
-port = process.env.PORT || 3001;
+  app = express(),
+  port = process.env.PORT || 3001;
 var Keyword = require('./api/models/keywordModel');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
@@ -9,38 +9,40 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/keyword');
 
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-   // Website you wish to allow to connect
-   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-   // Request methods you wish to allow
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
- 
-   // Request headers you wish to allow
-   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
- 
-   // Set to true if you need the website to include cookies in the requests sent
-   // to the API (e.g. in case you use sessions)
-   res.setHeader('Access-Control-Allow-Credentials', true);
- 
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  
-   // Pass to next layer of middleware
-   next();
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+
+
+  // Pass to next layer of middleware
+  next();
 
   // res.status(404).send({url: req.originalUrl + ' not found'});
 
-  
+
 });
 
-app.get('/404', function(req, res){
+app.get('/404', function (req, res) {
   throw new NotFound;
 });
 
-app.get('/500', function(req, res){
+app.get('/500', function (req, res) {
   throw new Error('keyboard cat!');
 });
 
